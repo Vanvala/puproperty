35aac0f7db8149d7f76bd4be64b26b76d0e9cb80
Vanvala <geos79@tut.by>
2022-03-17 23:45:42 +0300

    create plugin

---
+++ puproperty.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: puProperty
+Plugin URI: https://agbuda.by/
+Description: First Plugin
+Version: 1.0
+Author: Vanvala
+Author URI: https://agbuda.by/
+Licence: GPLv2 or later
+Text Domain: puproperty
+ */
